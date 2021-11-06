499e692e5d04e51f65114dc948c43912999ddcd8
Moetaz Jebri <60217228+MoetazJebri2307@users.noreply.github.com>
2021-11-06 17:19:17 +0100

    Moetaz

---
+++ Health+/app/src/main/java/eu/berdosi/app/heartbeat/Calendrier.java
@@ -0,0 +1,16 @@
+package eu.berdosi.app.heartbeat;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calendrier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendrier);
+    }
+}
---
+++ Health+/app/src/main/java/eu/berdosi/app/heartbeat/StepListener.java
@@ -0,0 +1,7 @@
+package eu.berdosi.app.heartbeat;
+
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
